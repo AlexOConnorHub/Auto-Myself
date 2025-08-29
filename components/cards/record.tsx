1d375fd8e6268c4ba10f422f3e768f5333107ac9
AlexOConnorHub <43659595+AlexOConnorHub@users.noreply.github.com>
2025-08-28 21:20:40 -0400

    Show user's prefered distance unit

--- components/cards/record.tsx
+++ components/cards/record.tsx
@@ -5,15 +5,18 @@ import { router } from 'expo-router';
 import ConditionalText from '@app/components/conditionalText';
 import Accordion from '../accordion';
 import { OptionButtons } from '../optionButtons';
+import { useDistanceUnit } from '../distanceUnit';
 
 export default function RecordCard(props): React.ReactElement {
   const record = props.record;
 
+  const distanceUnit = useDistanceUnit();
+
   return (
     <Accordion title={[record.date, record.type].join(' ')}>
       <View style={pageStyles.cardRow}>
-        <ConditionalText condition={record.odometer}>Odometer: {record.odometer}</ConditionalText>
-        <ConditionalText condition={record.interval}>Maintenance Interval: {record.interval} {record.interval_unit}</ConditionalText>
+        <ConditionalText condition={record.odometer}>Odometer: {record.odometer} {distanceUnit}</ConditionalText>
+        <ConditionalText condition={record.interval}>Maintenance Interval: {record.interval} {record.interval_unit === 'dist' ? distanceUnit : record.interval_unit}</ConditionalText>
         <ConditionalText condition={record.cost}>Cost: {record.cost}</ConditionalText>
         <ConditionalText condition={record.notes}>{record.notes}</ConditionalText>
         <OptionButtons
